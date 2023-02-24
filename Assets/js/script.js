b40fb296b36c63a7e6c1ff1141df4a5220027e8e
Mgamboa <marianellag400@gmail.com>
2023-02-24 15:47:36 -0600

    saving progress

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,11 +1,34 @@
 // console.log("hello world");
+var firstBox = document.querySelector(".first");
+var submitBtn = document.getElementById("submit");
+var thankYouBox = document.querySelector(".second");
+var rateAgain = document.getElementById("again");
+var numRating = document.getElementById("number");
+var rates = document.querySelectorAll(".btn");
 
-document.getElementById("submit").addEventListener("click", myFunction);
+submitBtn.addEventListener("click", () => {
+    thankYouBox.style.display = "block";
+    firstBox.style.display = "none";
+})
 
-function myFunction() {
-document.getElementById("two").style.display = "block";
-document.getElementById("one").style.display = "none";
-}
+rateAgain.addEventListener("click", () => {
+    thankYouBox.style.display = "none";
+    firstBox.style.display = "block";
+})//same code as above, just switching the outcome
+
+// document.getElementById("submit").addEventListener("click", myFunction);
+
+// function myFunction() {
+
+// document.getElementById("two").style.display = "block";
+// document.getElementById("one").style.display = "none";
+// if (rateAgain){
+//     document.getElementById("two").style.display = "none";
+//     document.getElementById("one").style.display = "block";
+// }
+// }
+
+
 // function rate () {
     
 // }

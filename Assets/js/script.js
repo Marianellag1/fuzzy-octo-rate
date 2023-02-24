5276684d2cf4c2556c897844157f429a12719b03
Mgamboa <marianellag400@gmail.com>
2023-02-24 13:34:49 -0600

    saving progress

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,24 @@
-console.log("hello world");
+// console.log("hello world");
 
-var btnOne = true;
+document.getElementById("submit").addEventListener("click", myFunction);
+
+function myFunction() {
+document.getElementById("two").style.display = "block";
+document.getElementById("one").style.display = "none";
+}
+// function rate () {
+    
+// }
+
+// rate();
+
+
+// 28-mini project- function- ability to vote again
+// var playAgain = confirm("Do you want to play again?");
+// console.log(playAgain);
+
+// if (playAgain) { //playAgain === true
+//    game();
+// }else {
+//     alert("Thank you for playing")
+// }

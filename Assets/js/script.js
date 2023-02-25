21ef381636899d192c2adb3f5e51c659c8b30fef
Mgamboa <marianellag400@gmail.com>
2023-02-25 17:00:23 -0600

    saving progress

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -6,14 +6,14 @@ var rateAgain = document.getElementById("again");
 var numRating = document.getElementById("number");
 var rates = document.querySelectorAll(".btn");
 
-submitBtn.addEventListener("click", () => {
-    thankYouBox.style.display = "block";
-    firstBox.style.display = "none";
+submitBtn.addEventListener("click", function () {//trying both ways of doing function() to get more confortable
+    thankYouBox.style.display = "block";//show
+    firstBox.style.display = "none";//no show
 })
 
 rateAgain.addEventListener("click", () => {//arrow function same as function(){}
-    thankYouBox.style.display = "none";
-    firstBox.style.display = "block";
+    thankYouBox.style.display = "none";//no show
+    firstBox.style.display = "block";//show
 })//same code as above, just switching the outcome
 
 rates.forEach( function (rate) {
@@ -24,16 +24,3 @@ rates.forEach( function (rate) {
     })
 })
 
-// document.getElementById("submit").addEventListener("click", myFunction);
-
-// function myFunction() {
-
-// 28-mini project- function- ability to vote again
-// var playAgain = confirm("Do you want to play again?");
-// console.log(playAgain);
-
-// if (playAgain) { //playAgain === true
-//    game();
-// }else {
-//     alert("Thank you for playing")
-// }
